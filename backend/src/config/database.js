b24b3bc311cc940aed7e6eb9f24b02b443a92923
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-07-05 23:47:16 +0530

    updated the db config

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 
 async function connectToDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI) 
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("CONNECTED TO DATABASE")
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     }
 }
